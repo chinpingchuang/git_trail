17b5e48877177d80cd4f0a0963186ac976673ae0
cpchuang <grey.chuang@gmail.com>
2015-07-13 07:47:50 +0000

    xxx

--- test.php
+++ test.php
@@ -3,7 +3,8 @@
 // Press the 'Run' button on the top to start the web server,
 // then click the URL that is emitted to the Output tab of the console
 
-echo 'Hello world from Cloud9!';
+echo 'Hello Kitty from Cloud9!';
 echo 'test' ;
 echo 'test' ;
+
 ?>
